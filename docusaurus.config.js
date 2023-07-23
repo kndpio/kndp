7918e2a33ae4a72a246f7d84d159ccda94ebc8c7
Enotmd <Enotmd@users.noreply.github.com>
2023-07-23 18:08:12 +0000

    deploy: e353aa889b1dadea0749872da2c66aebb522693f

---
+++ docusaurus.config.js
@@ -0,0 +1,83 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'KNDP',
+  tagline: 'KNDP is cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://github.com/',
+  baseUrl: '/',    
+  organizationName: 'web-seven', // Usually your GitHub org/user name.
+  projectName: 'kndp', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  
+   
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'KNDP',
+        logo: {
+          alt: 'KNDP',
+          src: 'img/logo.svg',
+        },
+        
+      },
+      footer: {
+        style: 'dark',
+        
+        copyright: `Copyright © ${new Date().getFullYear()} KNDP, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports=config;
