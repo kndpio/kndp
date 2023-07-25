a380c8d28cd0f02875ac067022b20f0ecc4456a1
Catalin <catalincretu1234@gmail.com>
2023-07-25 12:10:36 +0300

    KNDP-20: added responsive to iframe

--- packages/kndp/charts/kndp/files/extension-vault.js
+++ packages/kndp/charts/kndp/files/extension-vault.js
@@ -1,19 +1,35 @@
 ((window) => {
-    const component = () => {
-      const iframeProps = {
-        src: "https://kndp.local/ui", 
-        width: "100%",
-        height: "890px", 
-      };
-      return React.createElement("iframe", iframeProps);
+  const component = () => {
+    const iframeRef = React.useRef(null);
+
+    const adjustIframeHeight = () => {
+      if (iframeRef.current) {
+        const windowHeight = window.innerHeight;
+        iframeRef.current.style.height = windowHeight + 'px';
+      }
     };
-  
-    window.extensionsAPI.registerSystemLevelExtension(
-      component,
-      "Vault",
-      "/vault",
-      "fa fa-key "
-    );
-  
-  })(window);
-  
+
+    React.useEffect(() => {
+      adjustIframeHeight(); 
+      window.addEventListener('resize', adjustIframeHeight);
+      return () => {
+        window.removeEventListener('resize', adjustIframeHeight);
+      };
+    }, []);
+
+    const iframeProps = {
+      src: "https://kndp.local/ui",
+      width: "100%",
+      height: "100%", 
+      ref: iframeRef, 
+    };
+    return React.createElement("iframe", iframeProps);
+  };
+
+  window.extensionsAPI.registerSystemLevelExtension(
+    component,
+    "Secrets",
+    "/vault",
+    "fa fa-key "
+  );
+})(window);
