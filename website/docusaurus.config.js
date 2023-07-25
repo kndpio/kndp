23bbb26e6050bf7767b18ecc2e49fe0b49cc6c70
Enotmd <dorin.bounegru@gmail.com>
2023-07-25 11:25:53 +0300

    KNDP-31: docusaurus website deploy action with kndp.io

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/',
+  url: 'https://kndp.io/',
   baseUrl: '/',    
   organizationName: 'web-seven', // Usually your GitHub org/user name.
   projectName: 'kndp', // Usually your repo name.
