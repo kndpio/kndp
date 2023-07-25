1baf49bc9005f700db260982c7c1d9693268ac44
Dorin Bounegru <109219657+Enotmd@users.noreply.github.com>
2023-07-25 11:27:03 +0300

    KNDP-31: website action for deploy to gh pages (#26)
    * KNDP-31: created DOCUSAURUS /website path
    
    * KNDP-31: docusaurus website deploy action
    
    * KNDP-31: docusaurus website deploy action with kndp.io

---
+++ website/docusaurus.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'KNDP',
+  tagline: 'KNDP is cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://kndp.io/',
+  baseUrl: '/',    
+  organizationName: 'web-seven', // Usually your GitHub org/user name.
+  projectName: 'kndp', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  
+   
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  
+
+};
+
+module.exports=config;
