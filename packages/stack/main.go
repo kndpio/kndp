42066eec04c8898b9cafbdd8885ddb8442f764f6
Catalin <catalincretu1234@gmail.com>
2023-08-01 17:17:17 +0300

    KNDP-36: fixed stacks operator issues

--- packages/stack/main.go
+++ packages/stack/main.go
@@ -10,6 +10,7 @@ import (
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"syscall"
 
@@ -24,9 +25,11 @@ type Manifest struct {
 type ManifestMetadata struct {
 	Name string `yaml:"name"`
 }
-
+// applyManifest applies or deletes the Kubernetes manifest content.
+// If delete is true, it deletes the resources using `kubectl delete`.
+// If delete is false, it applies the resources using `kubectl apply`.
 func applyManifest(content []byte, delete bool) error {
-	if delete {
+	if !delete {
 		cmd := exec.Command("kubectl", "delete", "-f", "-")
 		cmd.Stdin = bytes.NewReader(content)
 		cmd.Stdout = os.Stdout
@@ -38,6 +41,7 @@ func applyManifest(content []byte, delete bool) error {
 
 		fmt.Println("Deleted manifest successfully")
 	} else {
+		
 		yamlDocs := bytes.Split(content, []byte("---"))
 
 		for _, doc := range yamlDocs {
@@ -60,7 +64,8 @@ func applyManifest(content []byte, delete bool) error {
 
 	return nil
 }
-
+// getManifestMetadata extracts metadata information from the manifest content.
+// It returns the name, description, and status of the manifest.
 func getManifestMetadata(content []byte) (string, string, string) {
 	var data map[string]interface{}
 	err := yaml.Unmarshal(content, &data)
@@ -82,28 +87,50 @@ func getManifestMetadata(content []byte) (string, string, string) {
 
 	return "", "", "" 
 }
-
+// getStatus checks the status of an application set using `kubectl describe`.
+// It returns "Activated" if the status contains "Status: True", otherwise "Not Activated".
 func getStatus(applicationName string) string {
 	cmd := exec.Command("kubectl", "describe", "applicationset", applicationName)
 	output, err := cmd.Output()
 	if err != nil {
-		return "Not Activated" 
+		return "Deactivated" 
 	}
 
-	if strings.Contains(string(output), "Status:                True") {
+	if matched, _ := regexp.MatchString(`(?m)^Status:\s*True$`, string(output)); matched {
 		return "Activated"
 	}
 
-	return "Not Activated"
+	return "Activated"
 }
+// getFilesInDirectory returns a list of file names in the specified directory with the given suffix.
+func getFilesInDirectory(dirPath, suffix string) []string {
+	var fileNames []string
 
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		fmt.Printf("Error reading folder: %s\n", err)
+		return fileNames
+	}
 
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), suffix) {
+			fileNames = append(fileNames, file.Name())
+		}
+	}
 
-
-
-
-
+	return fileNames
+}
+// main is the entry point of the application.
+// It reads environment variables, starts the HTTP server, and watches the stacks directory for changes.
 func main() {
+	stacksDirectory := os.Getenv("STACKS_DIRECTORY")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" 
+	}
+	if stacksDirectory == "" {
+		stacksDirectory = ".platform/stacks" 
+	}
 	corsHandler := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -120,21 +147,20 @@ func main() {
 	}
 
 	go func() {
-		http.HandleFunc("/api/objects", func(w http.ResponseWriter, r *http.Request) {
+		http.HandleFunc("/api/stacks", func(w http.ResponseWriter, r *http.Request) {
 			if r.Method == http.MethodGet {
-				fileNames := getFilesInDirectory("./.platform/stacks", ".yaml")
+				fileNames := getFilesInDirectory(stacksDirectory, ".yaml")
 				var response []map[string]string
 	
 				for _, fileName := range fileNames {
 					id := strings.TrimSuffix(fileName, ".yaml")
-					filePath := filepath.Join("./.platform/stacks", fileName)
+					filePath := filepath.Join(stacksDirectory, fileName)
 					content, err := ioutil.ReadFile(filePath)
 					if err != nil {
 						w.WriteHeader(http.StatusInternalServerError)
 						fmt.Fprintf(w, "Error reading file: %s", err)
 						return
 					}
-	
 					name, description, status := getManifestMetadata(content)
 					response = append(response, map[string]string{
 						"id":   id,
@@ -143,14 +169,12 @@ func main() {
 						"status": status,
 					})
 				}
-
 				responseJSON, err := json.Marshal(response)
 				if err != nil {
 					w.WriteHeader(http.StatusInternalServerError)
 					fmt.Fprintf(w, "Error marshaling data: %s", err)
 					return
 				}
-
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusOK)
 				w.Write(responseJSON)
@@ -166,42 +190,33 @@ func main() {
 					return
 				}
 
-				filePath := filepath.Join("./.platform/stacks", reqBody.ID+".yaml")
+				filePath := filepath.Join(stacksDirectory, reqBody.ID+".yaml")
 				content, err := ioutil.ReadFile(filePath)
 				if err != nil {
 					w.WriteHeader(http.StatusNotFound)
 					fmt.Fprintf(w, "Error reading file: %s", err)
 					return
 				}
-
-				if reqBody.Activate {
-					err = applyManifest(content, false)
-				} else {
-					err = applyManifest(content, true)
-				}
-
+				err = applyManifest(content, reqBody.Activate)
 				if err != nil {
 					w.WriteHeader(http.StatusInternalServerError)
 					fmt.Fprintf(w, "Error applying: %s", err)
 					return
 					}
-
 					w.WriteHeader(http.StatusOK)
-					fmt.Fprint(w, "Deactivated")
+					fmt.Fprint(w, "StatusOK")
 				} else {
 					w.WriteHeader(http.StatusMethodNotAllowed)
 					fmt.Fprint(w, "Unsupported method")
 				}
 			})
-
 			corsWrappedHandler := corsHandler(http.DefaultServeMux)
 
-			if err := http.ListenAndServe(":8080", corsWrappedHandler); err != nil {
+			if err := http.ListenAndServe(":"+port, corsWrappedHandler); err != nil {
 				fmt.Printf("Error starting HTTP server: %s\n", err)
 				os.Exit(1)
 			}
 		}()
-
 		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
 			fmt.Printf("Error creating watcher: %s\n", err)
@@ -209,36 +224,17 @@ func main() {
 		}
 		defer watcher.Close()
 
-		folderPath := "./.platform/stacks"
+		folderPath := stacksDirectory
 		err = watcher.Add(folderPath)
 		if err != nil {
 			fmt.Printf("Error watching folder: %s\n", err)
 			os.Exit(1)
 		}
 
-		fmt.Println("Operator is running...")
-
+		fmt.Println("Operator is running on port...", port)
 		terminate := make(chan os.Signal, 1)
 		signal.Notify(terminate, syscall.SIGINT, syscall.SIGTERM)
 		<-terminate
-
-		fmt.Println("Received termination signal. Stopping the operator.")
+		fmt.Println(" Stopping the operator.")
 	}
 
-	func getFilesInDirectory(dirPath, suffix string) []string {
-		var fileNames []string
-
-		files, err := ioutil.ReadDir(dirPath)
-		if err != nil {
-			fmt.Printf("Error reading folder: %s\n", err)
-			return fileNames
-		}
-
-		for _, file := range files {
-			if !file.IsDir() && strings.HasSuffix(file.Name(), suffix) {
-				fileNames = append(fileNames, file.Name())
-			}
-		}
-
-		return fileNames
-	}
